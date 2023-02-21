8accc770fdf119dabb4dcb97c74b65273ca1aed7
PaulaTam <ptam4@sfsu.edu>
2023-02-20 18:36:21 -0800

    added pfp images

--- front-end/src/Components/js/HomeComponents/DirGrid.js
+++ front-end/src/Components/js/HomeComponents/DirGrid.js
@@ -6,24 +6,30 @@ const DirGrid = () => {
     return (
         <div class="directory">
             <div class="mariet" id="half">
-                <div class="icon" id="item">one</div>
-                <div class="link" id="item">
+                <div class="icon" id="dir-item">
+                    <img id="pfp" src="mhymariet.png" alt="mariet-pfp" />
+                </div>
+                <div class="link" id="dir-item">
                     <p>Test paragraph</p>
-                    <a href="/mariet"><Button>Click Here!</Button></a>
+                    <a href="/mariet"><Button id="mbtn">Click Here!</Button></a>
                 </div>
             </div>
             <div class="hano" id="half">
-                <div class="link" id="item">
+                <div class="link" id="dir-item">
                     <p>Test paragraph</p>
-                    <a href="/hano"><Button>Click Here!</Button></a>
+                    <a href="/hano"><Button id="hbtn">Click Here!</Button></a>
                 </div>
-                <div class="icon" id="item">two</div>
+                <div class="icon" id="dir-item">
+                    <img id="pfp" src="mhyhano.png" alt="hano-pfp" />
+                </div>
             </div>
             <div class="yaron" id="half">
-                <div class="icon" id="item">one</div>
-                <div class="link" id="item">
+                <div class="icon" id="dir-item">
+                    <img id="pfp" src="mhybola.png" alt="yaron-pfp" />
+                </div>
+                <div class="link" id="dir-item">
                     <p>Test paragraph</p>
-                    <a href="/yaron"><Button>Click Here!</Button></a>
+                    <a href="/yaron"><Button id="bbtn">Click Here!</Button></a>
                 </div>
             </div>
         </div>
