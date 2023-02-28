a6108bd1b1c52f863da88c813fdfefb555e45871
Elyssa Tapawan <elyssatapawan@gmail.com>
2023-02-27 19:07:47 -0800

    Changed banner to regular image, fixed responsiveness for image and subnav for the most part

--- front-end/src/Components/js/SubNav.js
+++ front-end/src/Components/js/SubNav.js
@@ -1,6 +1,6 @@
 import "../css/SubNav.css";
 import React from 'react';
-import { Row, Col, Container, Navbar, Button } from 'react-bootstrap';
+import {Container, Navbar, Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 
 const SubNav = () => {
@@ -9,22 +9,12 @@ const SubNav = () => {
 
     return (
         <>
-            <Navbar bg="light" expand="true" sticky="top" className="subnav-position flex-wrap opacity-75">
+            <Navbar bg="light" expand="true" sticky="top" className="subnav subnav-position me-auto opacity-75">
                     <Container className="justify-content-md-center">
-                        <Row>
-                            <Col md="auto" className="justify-content-md-center">
-                                <Button variant="outline-dark" size="lg" onClick={() => navigate('/')}>HOME</Button>
-                            </Col>
-                            <Col md="auto" className="justify-content-md-center">
-                                <Button variant="outline-dark" size="lg" onClick={() => navigate('/mariet')}>MARIET</Button>
-                            </Col>
-                            <Col md="auto" className="justify-content-md-center">
-                                <Button variant="outline-dark" size="lg" onClick={() => navigate('/hano')}>HANO</Button>
-                            </Col>
-                            <Col>
-                                <Button variant="outline-dark" size="lg" onClick={() => navigate('/yaron')}>YARON</Button>
-                            </Col>
-                        </Row>
+                        <Button className="responsive" variant="outline-dark" size="lg" onClick={() => navigate('/')}>HOME</Button>
+                        <Button className="responsive" variant="outline-dark" size="lg" onClick={() => navigate('/mariet')}>MARIET</Button>
+                        <Button className="responsive" variant="outline-dark" size="lg" onClick={() => navigate('/hano')}>HANO</Button>
+                        <Button className="responsive" variant="outline-dark" size="lg" onClick={() => navigate('/yaron')}>YARON</Button>
                     </Container>
             </Navbar>
         </>
--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,17 +1,20 @@
-// import "./Home.css"; 
+import "./Home.css"; 
 import React from 'react';
 import HomeLayout from '../Components/js/HomeComponents/HomeLayout';
 import SubNav from '../Components/js/SubNav';
 import DirGrid from '../Components/js/HomeComponents/DirGrid';
+import {Container} from 'react-bootstrap';
+
 
 const Home = () => {
   return (
     <>
-    <div className="text-center justify-content-md-center home-image" >
-      <SubNav/>
-    </div>
-    <HomeLayout />
-    <DirGrid />
+      <SubNav />
+      <Container className="text-center justify-content-md-center" >
+        <img className="banner-image" src="banner.jpg" alt="homebanner" height="674" width="1360"></img>
+      </Container>
+      <HomeLayout />
+      <DirGrid />
     </>
   );
 };
