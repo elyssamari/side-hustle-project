5049819f54e2302393b2fcff30aafe0efd132844
PaulaTam <ptam4@sfsu.edu>
2023-03-01 13:12:09 -0800

    added desc + rounded buttons

--- front-end/src/Components/js/HomeComponents/DirGrid.js
+++ front-end/src/Components/js/HomeComponents/DirGrid.js
@@ -10,13 +10,21 @@ const DirGrid = () => {
                     <img id="pfp" src="mhymariet.png" alt="mariet-pfp" />
                 </div>
                 <div class="link" id="dir-item">
-                    <p>Test paragraph</p>
+                    <h1>mariet</h1>
+                    <p>Hi, I’m Mariet or Mari for short!</p>
+                    <p>☽(*◕ヮ◕*)☆</p>
+                    <p>I like to sing, play games, and doodle! 
+                        Some of my goals is to hopefully get better at music and art in the future.
+                    ♪(´▽｀)✧</p>
+                    <p>Please check out my website below!</p>
                     <a href="/mariet"><Button id="link-btn">Click Here!</Button></a>
                 </div>
             </div>
             <div class="hano" id="half">
                 <div class="link" id="dir-item">
-                    <p>Test paragraph</p>
+                    <h1>hano</h1>
+                    <p>Hi, I’m Hano!</p>
+                    <p>I like cute things and doodling bunnies :)</p>
                     <a href="/hano"><Button id="link-btn">Click Here!</Button></a>
                 </div>
                 <div class="icon" id="dir-item">
@@ -28,7 +36,11 @@ const DirGrid = () => {
                     <img id="pfp" src="mhybola.png" alt="yaron-pfp" />
                 </div>
                 <div class="link" id="dir-item">
-                    <p>Test paragraph</p>
+                    <h1>yaron</h1>
+                    <p>Hi, I’m Yaron!</p>
+                    <p>I like to tippity tap tap with games and coding. ٩(◕‿◕｡)۶ 
+                        at this rate im gonna develop carpal tunnel</p>
+                    <p>also gacha addict (its not gambling i swear)</p>
                     <a href="/yaron"><Button id="link-btn">Click Here!</Button></a>
                 </div>
             </div>
