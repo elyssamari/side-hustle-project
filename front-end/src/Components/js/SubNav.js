dc121d3aea8282df59264b275b821b7ecf49035c
PaulaTam <ptam4@sfsu.edu>
2023-03-13 17:26:02 -0700

    added scrollToTop function + clean up

--- front-end/src/Components/js/SubNav.js
+++ front-end/src/Components/js/SubNav.js
@@ -1,7 +1,6 @@
 import "../css/SubNav.css";
 import React from 'react';
 import { Navbar, Container, Button } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
 
 const SubNav = (props) => {
 
@@ -11,14 +10,19 @@ const SubNav = (props) => {
             behavior: 'smooth',
         })
     }
-    
-    const navigate = useNavigate();
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+        })
+    }
 
     return (
         <>
             <Navbar bg="light" sticky="top" className="flexWrap subnav subnav-position me-auto opacity-75">
                     <Container className="justify-content-md-center collapse navbar-collapse">
-                        <Button className="responsive" variant="outline-dark" size="md" onClick={() => scrollToSection(props.home)}>HOME</Button>
+                        <Button className="responsive" variant="outline-dark" size="md" onClick={() => scrollToTop()}>HOME</Button>
                         <Button className="responsive" variant="outline-dark" size="md" onClick={() => scrollToSection(props.mariet)}>MARIET</Button>
                         <Button className="responsive" variant="outline-dark" size="md" onClick={() => scrollToSection(props.hano)}>HANO</Button>
                         <Button className="responsive" variant="outline-dark" size="md" onClick={() => scrollToSection(props.yaron)}>YARON</Button>
